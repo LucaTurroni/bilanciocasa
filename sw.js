b6b1b58241b2ad90a443ccc4a3f1bf73dd3ad808
LucaTurroni <luca.turroni85@gmail.com>
2026-04-02 21:54:43 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // Aggiorna CACHE_VERSION ad ogni release per forzare
 // il rinnovo della cache su tutti i dispositivi.
 
-var CACHE_VERSION = '1.1.1';
+var CACHE_VERSION = '1.1.2';
 var CACHE_NAME    = 'bilancio-casa-' + CACHE_VERSION;
 
 // File da mettere in cache al primo avvio
